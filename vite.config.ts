652f206c2b2b8cd0a816ecad9668b0621d966fbf
Nischal Basuti <nischalbasuti1@gmail.com>
2025-12-19 13:14:59 +0530

    pwa stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,61 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: "./",
   assetsInclude: ['**/*.png'],
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', '*.png'],
+      manifest: {
+        name: 'Bitman',
+        short_name: 'Bitman',
+        description: 'A retro-style game where you dodge bombs and teeth',
+        theme_color: '#191970',
+        background_color: '#191970',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: './',
+        start_url: './',
+        icons: [
+          {
+            src: './icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: './icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2,ttf}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 3000,
   },
